0d05c4e06e1f1749547d089d35a2916fa71e4a70
lua <lua@luathena>
2022-04-17 22:53:27 -0400

    move instead of replace?

--- src/first.rs
+++ src/first.rs
@@ -41,8 +41,8 @@ impl List {
 impl Drop for List {
     fn drop(&mut self) {
         let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        while let Link::More(boxed_node) = cur_link {
+            cur_link = boxed_node.next;
         }
     }
 }
